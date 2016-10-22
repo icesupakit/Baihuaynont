7e9671f4dbf3b93689f20e1b6b111f2349ae0720
ice_supakit <iceroman222@gmail.com>
2016-10-22 16:07:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baihuaynont/newmainactivity.java
@@ -0,0 +1,14 @@
+package com.example.baihuaynont;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newmainactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newactivity);
+    }
+}
